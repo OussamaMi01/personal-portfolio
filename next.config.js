b7f3c5a7153bbb821476a89e29243360a1ebfefc
OussamaMi01 <missaouioussama774@gmail.com>
2026-02-07 12:12:34 +0100

    Fix duplicate email handler and secure env vars

--- next.config.js
+++ next.config.js
@@ -1,3 +1,21 @@
 module.exports = {
+   env: {
+    // Only expose PUBLIC environment variables here
+    NEXT_PUBLIC_SITE_NAME: process.env.NEXT_PUBLIC_SITE_NAME,
+    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
+  },
+  webpack: (config) => {
+    // Remove sensitive env vars from webpack DefinePlugin
+    config.plugins.forEach((plugin) => {
+      if (plugin.constructor.name === 'DefinePlugin') {
+        delete plugin.definitions['process.env.EMAIL_USER'];
+        delete plugin.definitions['process.env.EMAIL_PASS'];
+        delete plugin.definitions['process.env.EMAIL_HOST'];
+        delete plugin.definitions['process.env.EMAIL_PORT'];
+        delete plugin.definitions['process.env.EMAIL_TO'];
+      }
+    });
+    return config;
+  },
   reactStrictMode: true,
-}
+};
